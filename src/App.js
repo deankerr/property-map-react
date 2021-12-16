dfd244307a1f77332e51f08859d285db6e18cdbc
Dean Kerr <dean.kerr@gmail.com>
2021-12-16 20:58:40 +1100

    Refactor Domain query to custom hook

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
 import './App.css'
-import { useState, useEffect } from 'react'
 
 import GoogleMapReact from 'google-map-react'
-import axios from 'axios'
 
-// Google Map
+import {useDomainSearchResults} from './hooks/domainAPI'
+
+// Google Maps
 const DEFAULT_CENTER = { lat: -37.767228, lng: 144.962176 }
 const DEFAULT_ZOOM = 14
 
-// Domain
-const DOMAIN_SEARCH_BASE_URL = 'https://api.domain.com.au/v1/listings/residential/_search'
 
 function App() {
 
-  const [results, setResults] = useState([])
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [error, setError] = useState(null)
-
-
-  useEffect(() => {
-
-    if (!isLoaded && !error) {
-
-      axios.post(DOMAIN_SEARCH_BASE_URL,
-        {
-          "listingType": "Rent",
-          "locations": [
-            {
-              "state": "VIC",
-              "suburb": "brunswick",
-            }
-          ],
-        },
-        {
-          headers: {
-            'X-API-KEY': process.env.REACT_APP_DOMAIN_API_KEY
-          }
-        }
-      )
-        .then(res => {
-          console.log('Domain Search Response:', res)
-          setResults(res)
-          setIsLoaded(true)
-        })
-        .catch(err => {
-          console.log('Domain Search ERROR:', err.message)
-          setError(err)
-        })
-    }
-
-  })
+  const { results, isLoaded } = useDomainSearchResults()
 
 
   return (
---
+++ src/hooks/domainAPI.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
+
+const DOMAIN_SEARCH_BASE_URL = 'https://api.domain.com.au/v1/listings/residential/_search'
+
+function useDomainSearchResults() {
+
+  const [results, setResults] = useState([])
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [error, setError] = useState(null)
+
+
+  useEffect(() => {
+
+    if (!isLoaded) {
+
+      axios.post(DOMAIN_SEARCH_BASE_URL,
+        {
+          "listingType": "Rent",
+          "locations": [
+            {
+              "state": "VIC",
+              "suburb": "brunswick",
+            }
+          ],
+        },
+        {
+          headers: {
+            'X-API-KEY': process.env.REACT_APP_DOMAIN_API_KEY
+          }
+        }
+      )
+        .then(res => {
+          console.log('Domain Search Response:', res)
+          setResults(res)
+          setIsLoaded(true)
+        })
+        .catch(err => {
+          console.log('Domain Search ERROR:', err.message)
+          setError(err)
+        })
+      
+    }
+
+  }, [])
+
+  return { results, isLoaded, error }
+}
+
+export { useDomainSearchResults }
