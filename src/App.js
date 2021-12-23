e1cafebb2c12b42509a4f22abfdaebdf3ae52541
Dean Kerr <dean.kerr@gmail.com>
2021-12-23 17:24:06 +1100

    Add home page message

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
           </Route>
 
           <Route exact path="/">
-            
+            <p>Select an option above to begin!</p>
           </Route>
 
           <Route path="*">
