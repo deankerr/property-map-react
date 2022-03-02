d2d58a03a340d25b9aa7d77bb9c88ce517ca46e3
Dean Kerr <dean.kerr@gmail.com>
2022-03-03 02:46:23 +1100

    Delete Rent and Buy as they now exist as routes

--- src/components/Buy.js
+++
@@ -1,7 +0,0 @@
-import Search from './Search'
-
-export default function Buy(props) {
-  const { searchCache, saveSearchCache } = props
-
-  return <Search searchCache={searchCache} saveSearchCache={saveSearchCache} listingType="Sale" />
-}
--- src/components/Rent.js
+++
@@ -1,7 +0,0 @@
-import Search from './Search'
-
-export default function Rent(props) {
-  const { searchCache, saveSearchCache } = props
-
-  return <Search searchCache={searchCache} saveSearchCache={saveSearchCache} listingType="Rent" />
-}
