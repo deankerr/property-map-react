410605dbf7a44e077597dbea6b8b7c8480c34736
Dean Kerr <dean.kerr@gmail.com>
2022-03-02 15:34:27 +1100

    Create main app layout

--- src/App.js
+++ src/App.js
@@ -1,62 +1,74 @@
-// import './normalize.css'
 import './App.css'
-import { useState } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import { useState } from 'react'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import Navigation from './components/Navigation'
-import Listing from './components/Listing'
-import Favourites from './components/Favourites'
-import Buy from './components/Buy'
-import Rent from './components/Rent'
+import { Container, Row, Col } from 'react-bootstrap'
 
-function App() {
-  const [searchCache, setSearchCache] = useState({})
-  function saveSearchCache(data) {
-    console.log(`Saving to searchCache`, data)
-    setSearchCache(data)
-  }
+import Map from './components/Map'
+// import Navigation from './components/Navigation'
+// import Listing from './components/Listing'
+// import Favourites from './components/Favourites'
 
-  const [listingCache, setListingCache] = useState([])
-
-  function saveListingToCache(data) {
-    console.log(`Saving to listingCache id: ${data.id}`)
-    const newCache = [...listingCache, data]
-    console.log(`New cache is:`, newCache)
-    setListingCache(newCache)
-  }
-
+export default function App() {
   return (
-    <Router>
-      <h1 className="display-3 text-center">Propertify</h1>
-      <Navigation />
+    <Container fluid>
+      <Row>
+        <Col lg={4} className="justify-content-center">
+          <h1 className="text-center">Propertify</h1>
+        </Col>
 
-      <Switch>
-        <Route path="/listing/:id">
-          <Listing listingCache={listingCache} saveListingToCache={saveListingToCache} searchCache={searchCache} />
-        </Route>
-
-        <Route path="/rent">
-          <Rent searchCache={searchCache} saveSearchCache={saveSearchCache} />
-        </Route>
-
-        <Route path="/buy">
-          <Buy searchCache={searchCache} saveSearchCache={saveSearchCache} />
-        </Route>
-
-        <Route path="/favourites">
-          <Favourites />
-        </Route>
-
-        <Route exact path="/">
-          <h4 className="text-center">Select an option above to begin!</h4>
-        </Route>
-
-        <Route path="*">
-          <h4 className="text-center">Page not found</h4>
-        </Route>
-      </Switch>
-    </Router>
+        <Col lg={8} className="px-0">
+          <Map />
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
-export default App
+//   const [searchCache, setSearchCache] = useState({})
+//   function saveSearchCache(data) {
+//     console.log(`Saving to searchCache`, data)
+//     setSearchCache(data)
+//   }
+
+//   const [listingCache, setListingCache] = useState([])
+
+//   function saveListingToCache(data) {
+//     console.log(`Saving to listingCache id: ${data.id}`)
+//     const newCache = [...listingCache, data]
+//     console.log(`New cache is:`, newCache)
+//     setListingCache(newCache)
+//   }
+
+//   return (
+//     <Router>
+//       <h1 className="display-3 text-center">Propertify</h1>
+//       <Navigation />
+
+//       <Switch>
+//         <Route path="/listing/:id">
+//           <Listing listingCache={listingCache} saveListingToCache={saveListingToCache} searchCache={searchCache} />
+//         </Route>
+
+//         <Route path="/rent">
+//           <Rent searchCache={searchCache} saveSearchCache={saveSearchCache} />
+//         </Route>
+
+//         <Route path="/buy">
+//           <Buy searchCache={searchCache} saveSearchCache={saveSearchCache} />
+//         </Route>
+
+//         <Route path="/favourites">
+//           <Favourites />
+//         </Route>
+
+//         <Route exact path="/">
+//           <h4 className="text-center">Select an option above to begin!</h4>
+//         </Route>
+
+//         <Route path="*">
+//           <h4 className="text-center">Page not found</h4>
+//         </Route>
+//       </Switch>
+//     </Router>
+//   )
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,63 +1,75 @@
 import GoogleMapReact from 'google-map-react'
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 
-import { useSearchResults } from '../hooks/domainAPI'
-import PropertyMapMarker from './PropertyMapMarker'
+// import { useSearchResults } from '../hooks/domainAPI'
+// import PropertyMapMarker from './PropertyMapMarker'
 
 // Google Maps
 const DEFAULT_CENTER = { lat: -37.8136, lng: 144.9631 }
 const DEFAULT_ZOOM = 16
 
 export default function Map(props) {
-  const { searchCache, saveSearchCache, formQuery, listingType } = props
-
-  const { results, isLoaded, clearResults } = useSearchResults(formQuery, searchCache, saveSearchCache)
-
-  const [center, setCenter] = useState(DEFAULT_CENTER)
-  useEffect(() => {
-    // Find a rough "centre point" of the results by averaging the lat/lngs, so we can zoom the map there
-    if (isLoaded && results.length > 0) {
-      let lat = 0,
-        lng = 0
-      results.forEach((res) => {
-        lat += res.listing.propertyDetails.latitude
-        lng += res.listing.propertyDetails.longitude
-      })
-      lat = lat / results.length
-      lng = lng / results.length
-      setCenter({ lat, lng })
-    }
-  }, [isLoaded])
-
-  // Erase results if we just changed from rent <-> buy
-  useEffect(() => {
-    clearResults()
-    saveSearchCache({})
-  }, [listingType])
-
   return (
-    <>
-      <div className="googleMap">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
-          center={center}
-          zoom={DEFAULT_ZOOM}
-        >
-          {isLoaded &&
-            results.map((res) => {
-              return (
-                <PropertyMapMarker
-                  lat={res.listing.propertyDetails.latitude}
-                  lng={res.listing.propertyDetails.longitude}
-                  headline={res.listing.headline}
-                  image={res.listing.media[0].url}
-                  id={res.listing.id}
-                  key={res.listing.id}
-                />
-              )
-            })}
-        </GoogleMapReact>
-      </div>
-    </>
+    <div className="googleMap">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        center={DEFAULT_CENTER}
+        zoom={DEFAULT_ZOOM}
+      >
+        <p>Map</p>
+      </GoogleMapReact>
+    </div>
   )
 }
+
+// const { searchCache, saveSearchCache, formQuery, listingType } = props
+
+// const { results, isLoaded, clearResults } = useSearchResults(formQuery, searchCache, saveSearchCache)
+
+// const [center, setCenter] = useState(DEFAULT_CENTER)
+// useEffect(() => {
+//   // Find a rough "centre point" of the results by averaging the lat/lngs, so we can zoom the map there
+//   if (isLoaded && results.length > 0) {
+//     let lat = 0,
+//       lng = 0
+//     results.forEach((res) => {
+//       lat += res.listing.propertyDetails.latitude
+//       lng += res.listing.propertyDetails.longitude
+//     })
+//     lat = lat / results.length
+//     lng = lng / results.length
+//     setCenter({ lat, lng })
+//   }
+// }, [isLoaded])
+
+// // Erase results if we just changed from rent <-> buy
+// useEffect(() => {
+//   clearResults()
+//   saveSearchCache({})
+// }, [listingType])
+
+// return (
+//   <>
+//     <div className="googleMap">
+//       <GoogleMapReact
+//         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+//         center={center}
+//         zoom={DEFAULT_ZOOM}
+//       >
+//         {isLoaded &&
+//           results.map((res) => {
+//             return (
+//               <PropertyMapMarker
+//                 lat={res.listing.propertyDetails.latitude}
+//                 lng={res.listing.propertyDetails.longitude}
+//                 headline={res.listing.headline}
+//                 image={res.listing.media[0].url}
+//                 id={res.listing.id}
+//                 key={res.listing.id}
+//               />
+//             )
+//           })}
+//       </GoogleMapReact>
+//     </div>
+//   </>
+// )
